56adf8dfc6147092baa95047c02ebd0f3307fc0c
Maha <eneyed@gmail.com>
2014-06-18 11:34:03 +0530

    maha | adding sample migrations with dummy table

--- src/main/java/app/ApplicationService.java
+++ src/main/java/app/ApplicationService.java
@@ -4,22 +4,39 @@ import app.scheduler.JobScheduler;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.db.DatabaseConfiguration;
+import com.yammer.dropwizard.jdbi.DBIFactory;
+import com.yammer.dropwizard.migrations.MigrationsBundle;
 import de.spinscale.dropwizard.jobs.JobsBundle;
+import org.skife.jdbi.v2.DBI;
 
 
 public class ApplicationService extends Service<MigratorConfiguration> {
+
+    private final String contentMigrator = "ContentMigrator";
+
     public static void main(String[] args) throws Exception {
         new ApplicationService().run(args);
     }
 
     @Override
     public void initialize(Bootstrap<MigratorConfiguration> bootstrap) {
-        bootstrap.setName("ContentMigrator");
+        bootstrap.setName(contentMigrator);
+
         bootstrap.addBundle(new JobsBundle("app"));
+        bootstrap.addBundle(new MigrationsBundle<MigratorConfiguration>() {
+            @Override
+            public DatabaseConfiguration getDatabaseConfiguration(MigratorConfiguration configuration) {
+                return configuration.getDatabase();
+            }
+        });
     }
 
     @Override
     public void run(MigratorConfiguration configuration, Environment environment) throws Exception {
+        final DBIFactory factory = new DBIFactory();
+        final DBI jdbi = factory.build(environment, configuration.getDatabase(), contentMigrator);
+
         new JobScheduler().doJob();
     }
 }
--- src/main/java/app/MigratorConfiguration.java
+++ src/main/java/app/MigratorConfiguration.java
@@ -1,33 +1,28 @@
 package app;
 
 import com.yammer.dropwizard.config.Configuration;
+import com.yammer.dropwizard.db.DatabaseConfiguration;
 import org.codehaus.jackson.annotate.JsonProperty;
-import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class MigratorConfiguration extends Configuration{
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
+    @Valid
+    @NotNull
+    @JsonProperty
     private String defaultName = "Stranger";
 
+    @Valid
+    @NotNull
     @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
+    private DatabaseConfiguration database = new DatabaseConfiguration();
 
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
     public String getDefaultName() {
         return defaultName;
     }
 
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
+    public DatabaseConfiguration getDatabase() {
+        return database;
     }
 }
