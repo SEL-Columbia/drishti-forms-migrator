bea74b69881a3a8ee750211dcba44f210c79eca5
Maha <eneyed@gmail.com>
2014-07-07 17:38:31 +0530

    maha | making http call fail safe

--- src/main/java/app/util/HttpClient.java
+++ src/main/java/app/util/HttpClient.java
@@ -1,8 +1,11 @@
 package app.util;
 
+import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
 import java.net.URI;
@@ -10,12 +13,19 @@ import java.util.List;
 import java.util.Map;
 
 public class HttpClient {
+    private static Logger logger = LoggerFactory.getLogger(HttpClient.class);
 
     public static List<Map<String, Object>> call(URI uri, String username, String password) {
         Client client = Client.create(HttpClientHelper.configureClient());
         client.addFilter(new HTTPBasicAuthFilter(username, password));
         WebResource service = client.resource(uri);
 
-        return (List<Map<String, Object>>) service.accept(MediaType.APPLICATION_JSON).get(List.class);
+        try {
+            return (List<Map<String, Object>>) service.accept(MediaType.APPLICATION_JSON).get(List.class);
+        } catch (Exception e) {
+            logger.error("Could not make http call : " + e.getMessage());
+        }
+
+        return Lists.newArrayList();
     }
 }
